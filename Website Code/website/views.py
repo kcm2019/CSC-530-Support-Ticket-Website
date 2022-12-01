74f0e02b1245405fc6dfe3c268b6ab8c83af788e
kurtmuller-studnet <65182311+kcm2019@users.noreply.github.com>
2022-12-01 10:54:14 -0500

    Added Admin Editing Incidents
    - Added Admin Editing Incidents
       - Removed Admin Adding Incidents
       - Need to have checks for if the info is correct
       - Need to add admin_id to note table and have it displayed where necessary

--- Website Code/website/views.py
+++ Website Code/website/views.py
@@ -14,16 +14,39 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if (current_user.admin_access == True):
-        if request.method == 'POST':
+        """if request.method == 'POST': # and request.form["note-submit"]
             note = request.form.get('note')
 
             if len(note) < 1:
-                flash('Note is too short', category='error')
+                flash('Comment is too short', category='error')
             else:
-                new_note = Note(data=note, user_id=current_user.id)
+                new_note = Note(data=note, user_id=current_user.id, status="Open")
                 db.session.add(new_note)
                 db.session.commit()
-                flash('Note Added!', category='success')
+                flash('Incident Added!', category='success') """
+        if request.method == 'POST': # and request.form["note-edit"]
+            note_id = request.form.get('note_id')
+            admin_comments = request.form.get("admin_comments")
+            status = request.form.get("note_status")
+            note = Note.query.filter_by(id=note_id).first()
+            print("*****NOTE EDITING IN ADMIN VIEW******")
+            print(note)
+            print(type(note))
+            if len(admin_comments) < 1:
+                flash("Comment is too short", category='error')
+            #elif Note.query.filter_by(id=note_id).first() != note_id: #Checks if note exists
+                #flash("Incident Does Not Exist", category='error')
+            #elif status != "Open" or status != "Closed" or status != "In Progress":
+                #flash("Status must be Open, Closed, or In Progress", category='error')
+            else:
+                updated_note = Note(id=note_id, data=note.data, user_id=note.user_id, admin_notes=admin_comments, status=status)
+
+                db.session.delete(note)
+                db.session.commit()
+
+                db.session.add(updated_note)
+                db.session.commit()
+                flash('Incident Edited!', category='success')
         return render_template("admin.html", user=current_user)
     else: 
         if request.method == 'POST':
